49c2248dbf9a0f197fd9ab6701913662401f72b9
LaurentRDC <laurent.decotret@outlook.com>
2016-12-13 13:57:18 -0500

    Repository cleanup

--- benchmark.py
+++
@@ -1,8 +0,0 @@
-
-from dualtree import dualtree, idualtree
-import numpy as n
-
-if __name__ == '__main__':
-    signal = n.random.rand(1e4, 1e4)
-    transf = dualtree(signal, level = 'max')
-    recon = idualtree(transf)
