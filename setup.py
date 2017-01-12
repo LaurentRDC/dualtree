34dcff98e4d1d2c2fade88656cf39583c9feafd9
LaurentRDC <laurent.decotret@outlook.com>
2017-01-12 18:55:43 -0500

    FIX: dependency minimum for PyWavelets

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open(join(dirname(__file__), 'LICENSE.txt')) as license_file:
     lic = license_file.read().replace('\n', '')
 
 setup(name = 'dualtree',
-      version = '0.7',
+      version = '0.7.1',
       description = 'Dual-tree complex wavelet transform and associated algorithms',
       author = 'Laurent P. René de Cotret',
       author_email = 'laurent.renedecotret@mail.mcgill.ca',
       url = 'http://www.physics.mcgill.ca/siwicklab/software.html',
       packages = ['dualtree', 'dualtree.tests'],
-      install_requires = ['numpy', 'PyWavelets'],
+      install_requires = ['numpy', 'PyWavelets >= 0.5.1'],
       package_data = {'dualtree': ['*.txt'],
                       'dualtree': ['data/*.npz']},
       license = lic
